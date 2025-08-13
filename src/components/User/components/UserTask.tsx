67c099af95f01302147afba7935a7b90a7042ca9
Zafeer <mohammadzafeer2610@gmail.com>
2025-08-13 14:58:15 +0530

    fixed text oon task section

--- src/components/User/components/UserTask.tsx
+++ src/components/User/components/UserTask.tsx
@@ -6,8 +6,8 @@ const UserTask = () => {
   return (
     <div className='flex flex-col gap-6'>
       <div className='flex flex-col items-start gap-2 px-5'>
-        <h1 className='text-xl font-semibold'>Task Dashboard</h1>
-        <span className='text-sm'>View and manage assign tasks abd purchase orders</span>
+        <h1 className='text-xl font-semibold text-start'>Task Dashboard</h1>
+        <span className='text-sm text-start'>View and manage assign tasks and purchase orders</span>
       </div>
       <UserTaskCards />
       <TaskCard/>
