95d1911ba432b88ec921488cd076646d1f105fc8
Zafeer <mohammadzafeer2610@gmail.com>
2025-08-13 11:26:55 +0530

    added time frame login for user only and added update permission UI with View Report at UserList section on admin panel

--- src/components/Admin/component/UserReport.tsx
+++ src/components/Admin/component/UserReport.tsx
@@ -32,7 +32,7 @@ const UserReport: React.FC<UserReportProps> = ({
   role,
   onClose,
 }) => (
-  <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8 flex flex-col items-center space-y-6 relative">
+  <div className="lg:w-lg xl:w-lg w-sm bg-white rounded-xl shadow-lg p-8 flex flex-col items-center space-y-6 relative border">
     {onClose && (
       <button
         className="absolute top-2 right-2 text-gray-500 hover:text-gray-800 text-2xl font-bold z-10"
@@ -58,19 +58,19 @@ const UserReport: React.FC<UserReportProps> = ({
     </div>
     <div className="w-full grid grid-cols-2 gap-4 mt-4">
       <div className="bg-blue-50 rounded-lg p-4 text-center">
-        <div className="text-lg font-bold text-blue-700">{orderCount}</div>
+        <div className="text-lg font-bold text-blue-700">45</div>
         <div className="text-xs text-gray-500">Total PO Generated</div>
       </div>
       <div className="bg-green-50 rounded-lg p-4 text-center">
-        <div className="text-lg font-bold text-green-700">{completedPO}</div>
+        <div className="text-lg font-bold text-green-700">33</div>
         <div className="text-xs text-gray-500">Completed PO</div>
       </div>
       <div className="bg-yellow-50 rounded-lg p-4 text-center">
-        <div className="text-lg font-bold text-yellow-700">{pendingPO}</div>
+        <div className="text-lg font-bold text-yellow-700">12</div>
         <div className="text-xs text-gray-500">Pending PO</div>
       </div>
       <div className="bg-red-50 rounded-lg p-4 text-center">
-        <div className="text-lg font-bold text-red-700">{delayedPO}</div>
+        <div className="text-lg font-bold text-red-700">2</div>
         <div className="text-xs text-gray-500">Delayed PO</div>
       </div>
     </div>
@@ -79,7 +79,7 @@ const UserReport: React.FC<UserReportProps> = ({
         <span>Last Login:</span>
         <span className="font-medium">{lastLogin}</span>
       </div>
-      <div className="flex justify-between text-sm text-gray-600">
+      {/* <div className="flex justify-between text-sm text-gray-600">
         <span>Status:</span>
         <span
           className={`font-medium ${
@@ -88,6 +88,28 @@ const UserReport: React.FC<UserReportProps> = ({
         >
           {Isverified ? "Verified" : "Unverified"}
         </span>
+      </div> */}
+      <div className="flex flex-col items-start gap-2">
+        <span className="text-start w-full">Permissions : </span>
+        <div className="flex gap-3">
+          <div className="flex items-center gap-1">
+            <input id="readOnly" type="checkbox" />
+            <label htmlFor="readOnly">Read</label>
+          </div>
+          <div className="flex items-center gap-1">
+            <input id="create" type="checkbox" />
+            <label htmlFor="create">Create</label>
+          </div>
+          <div className="flex items-center gap-1">
+            <input id="update" type="checkbox" />
+            <label htmlFor="update">Update</label>
+          </div>
+          <div className="flex items-center gap-1">
+            <input id="delete" type="checkbox" />
+            <label htmlFor="delete">Delete</label>
+          </div>
+        </div>
+        <button className="bg-blue-500 px-2 py-1 text-sm text-white font-semibold rounded hover:bg-blue-600 cursor-pointer duration-300 active:bg-blue-500">Update Permissions</button>
       </div>
     </div>
   </div>
